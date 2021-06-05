4848e892d7d40245b5d1baa03bbbe12c92fbe8bb
Andrei Moldoveanu <moldoveanud.andrei@yahoo.com>
2021-06-05 21:03:03 +0300

    update log files

--- src/main/java/com/example/springproject/logData/PrintLog.java
+++ src/main/java/com/example/springproject/logData/PrintLog.java
@@ -2,6 +2,7 @@ package com.example.springproject.logData;
 
 import com.example.springproject.model.Address;
 
+import java.util.Iterator;
 import java.util.List;
 
 public class PrintLog {
@@ -15,16 +16,20 @@ public class PrintLog {
     public static void printLog(Address address, List<Address> correctAddressList) {
         LogInterface log = new EventLog();
         StringBuilder value = new StringBuilder();
-        value.append(">>> Received Address: \n");
+        value.append("\n>>> Received Address: \n");
         value.append(address.toString()).append("\n");
         value.append("------------------------------------\n");
-        value.append(">>> Corrected Address: \n");
-        for (Address correctAddress: correctAddressList) {
+        value.append("\n>>> Corrected Address: \n");
+        Iterator<Address> itCorrectAddress = correctAddressList.listIterator();
+        while(itCorrectAddress.hasNext()){
+            Address correctAddress = itCorrectAddress.next();
             value.append(correctAddress.toString()).append("\n");
+//            itCorrectAddress.next();
+            if(itCorrectAddress.hasNext())
+                value.append("---\n");
         }
         value.append("\n");
         value.append("======================================\n");
-        value.append("\n");
         log.printLogger(value.toString());
     }
 }
